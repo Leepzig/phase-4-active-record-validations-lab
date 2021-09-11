2956e139b5864333ca026f3e6aa0dd64c09c2c94
andrew <neon.duncan@gmail.com>
2021-09-11 09:55:27 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :click_bait_validation
+
+  def click_bait_validation
+    if !:title.match?(/Won't Believe|Secret|Top \d*|Guess/)
+      errors.add(:title, "Sorry you're title isn't clickbait-y enough")
+    end
+  end
 end
